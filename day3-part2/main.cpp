123fad04d6a35d1ea3d9726b5891f8f3b8fd2d98
Rutherford, Cameron <robert.rutherford@pnnl.gov>
2024-12-04 12:03:28 -0500

    Add day 3 part 2 skeleton code.

---
+++ day3-part2/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+
+int main() {
+  std::ifstream inputFile("input.txt");
+
+  if (!inputFile.is_open())
+  {
+    std::cerr << "Error opening file!" << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  int result = 0;
+
+  while (std::getline(inputFile, line))
+  {
+    std::smatch res;
+    std::regex rx("mul\\([0-9]+,[0-9]+\\)");
+    std::string::const_iterator start = line.cbegin();
+    // If dont() matches before the mul(), skip to next do()
+    // Does this track across lines? - yes
+    while(std::regex_search(start, line.cend(), res, rx))
+    {
+      result += std::stoi(res[0].str().substr(4, res[0].str().find(',') - 4)) * std::stoi(res[0].str().substr(res[0].str().find(',') + 1, res[0].str().size() - res[0].str().find(',') - 2));
+      start = res.suffix().first;
+    }
+  }
+
+  inputFile.close();
+
+  std::cout << "Answer: " << result << std::endl;
+  return 0;
+}
