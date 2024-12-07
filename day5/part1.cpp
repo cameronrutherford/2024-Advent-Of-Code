95480663b597eed757cf0a705d21a68a755d27a4
cameronrutherford <cameron.rutherford@me.com>
2024-12-07 17:29:19 -0500

    Add day 5 part 2 solution.

--- day5/part1.cpp
+++ day5/part1.cpp
@@ -4,33 +4,9 @@
 #include <string>
 #include <iterator>
 #include <vector>
-#include <queue>
 #include <ranges>
-#include <algorithm>
 #include <map>
 #include <set>
-
-bool is_valid(std::vector<int> update, std::map<int, std::vector<int>>& rules)
-{
-    bool valid = true;
-    for (int i = update.size() - 1; i >= 0; i--)
-    {
-      std::set<int> potential_conflicts(rules[update[i]].begin(), rules[update[i]].end());
-      for (int j = i - 1; j >= 0; j--)
-      {
-        if (potential_conflicts.contains(update[j]))
-        {
-          valid = false;
-          break;
-        }
-      }
-      if (!valid)
-      {
-        break;
-      }
-    }
-    return valid;
-}
 
 int main() {
 
@@ -47,7 +23,6 @@ int main() {
   std::string line;
   std::map<int, std::vector<int>> rules;
   std::vector<std::vector<int>> updates;
-  std::vector<std::vector<int>> invalid_updates;
 
   while (std::getline(inputFile, line))
   {
@@ -89,45 +64,29 @@ int main() {
 
   for (auto update : updates)
   {
-    if (!is_valid(update, rules))
+    bool valid = true;
+    for (int i = update.size() - 1; i >= 0; i--)
     {
-      invalid_updates.push_back(update);
+      std::set<int> potential_conflicts(rules[update[i]].begin(), rules[update[i]].end());
+      for (int j = i - 1; j >= 0; j--)
+      {
+        if (potential_conflicts.contains(update[j]))
+        {
+          valid = false;
+          break;
+        }
+      }
+      if (!valid)
+      {
+        break;
+      }
+    }
+    if (valid)
+    {
+      result += *(update.begin() + update.size() / 2);
     }
   }
-
-  for (auto update : invalid_updates)
-  {
-    auto cmp = [&rules](int left, int right)
-    { 
-      if (std::find(rules[left].begin(), rules[left].end(), right) != rules[left].end())
-      {
-        return true;
-      }
-      else if (std::find(rules[right].begin(), rules[right].end(), left) != rules[right].end())
-      {
-        return false;
-      }
-      else
-        return left < right;
-    };
-    std::priority_queue<
-      int, 
-      std::vector<int>, 
-      decltype(cmp)
-      >
-    sorted_updates(cmp);
-    for (auto ele : update)
-      sorted_updates.push(ele);
-
-    std::vector<int> sorted_vector_update;
-    while (!sorted_updates.empty())
-    {
-      sorted_vector_update.push_back(sorted_updates.top());
-      sorted_updates.pop();
-    }
-    result += *(sorted_vector_update.begin() + sorted_vector_update.size() / 2);
-  }
-  // For update in invalid_updates
+  // For update in updates
   //    if update is valid given rules
   //        result += middle of update
 
